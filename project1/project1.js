ca7d1e2d59847e64507343cd8f29e3f265c02189
grace919 <hlomphomorojele067@gmail.com>
2025-01-23 16:56:19 +0200

    updated by meow

--- project1/project1.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const links = document.querySelectorAll('.category a'); 
-    links.forEach(link => { 
-        link.addEventListener('click', (event) => {
-            event.preventDefault(); 
-            alert(`You clicked on ${link.textContent}');
-        });
-    });
-});
